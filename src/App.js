d81b856b57118411c5b06fd46e9ac9abdd91e0b7
ReubenScott <reubenlscott@gmail.com>
2024-02-20 01:28:11 +0900

    UI Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './style/App.scss';
+
+import React, { Fragment } from 'react';
 // import logo from './logo.svg';
-import './App.css';
 import Search from "./components/Search";
+import axios from 'axios';
+import { Container, Row, Col } from 'reactstrap';
+
+import Post from './components/Post';
+import Header from './components/Header';
+import LeftCard from './components/LeftCard';
 
 
 function App() {
@@ -22,8 +30,27 @@ function App() {
             <li>three22</li>
         </ul>
 
-        <h1>Hello CodeSandbox</h1>
-        <h2>Start editing to see some magic happen!</h2>
+        <Fragment>
+
+            <Header />
+
+            <main className="my-5 py-5">
+                <Container className="px-0">
+                    <Row noGutters className="pt-2 pt-md-5 w-100 px-4 px-xl-0 position-relative">
+
+                        <Col xs={{ order: 2 }} md={{ size: 4, order: 1 }} tag="aside" className="pb-5 mb-5 pb-md-0 mb-md-0 mx-auto mx-md-0">
+                            <LeftCard />
+                        </Col>
+
+                        <Col xs={{ order: 1 }} md={{ size: 7, offset: 1 }} tag="section" className="py-5 mb-5 py-md-0 mb-md-0">
+                            <Post />
+                        </Col>
+
+                    </Row>
+                </Container>
+            </main>
+
+        </Fragment>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React,{StrictMode} from 'react';
 import ReactDOM from 'react-dom';
 
-import './index.css';
+import './style/index.scss';
 import App from './App';
 
 
